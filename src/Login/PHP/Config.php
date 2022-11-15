81f5ae1cdc92ea71e248b91ceaa07437110dd684
Iván Roch <ivan.roch.rodriguez@gmail.com>
2022-11-15 17:24:49 +0100

    Base de datos

---
+++ src/Login/PHP/Config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'localhost:3037');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', '');
+   define('DB_DATABASE', 'TrabajoFinal');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
